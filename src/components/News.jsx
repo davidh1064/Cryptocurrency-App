5c7964bd7d719ad0a942eacf4e1b025b07b7508b
dhuang97 <huangdavid10@gmail.com>
2024-05-30 11:52:15 -0700

    Initial commit

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -1,34 +1,49 @@
-import React from 'react';
+import React from "react";
 import { Select, Typography, Row, Col, Avatar, Card } from "antd";
 import moment from "moment";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
-import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+const { Text, Title } = Typography;
+const { Option } = Select;
 
-const {Text, Title } = Typography; 
-const { Option } = Select; 
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 6 : 12,
+  });
 
-const News = ({simplified}) => {
-
-  const {data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12});
-
-if(!cryptoNews?.value) return "Loading...";
+  if (!cryptoNews?.data) return "Loading...";
 
   return (
-    <Row gutter={[ 24, 24]}>
-        {cryptoNews.value.map((news, i) => (
-            <Col xd = {24} sm = {12} lg ={8} key = {i}>
-                <Card hoverable className = "news-card">
-                  <a href = {news.url} target = "_blank" rel="norefferer">
-                      <div className = "news-image-container">
-                          <Title className = "news-title" level={4}>{news.name}</Title>
-                      </div>
-                  </a>
-                </Card>
-            </Col>
-
-        )) }
+    <Row gutter={[24, 24]}>
+      {cryptoNews.data.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.link} target="_blank" rel="noreferrer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.title}
+                </Title>
+                <img className="news-image" src={news.photo_url} alt="news" />
+              </div>
+              <p>
+                {news.description > 100
+                  ? `${news.description.substring(0, 100)}...`
+                  : news.description}
+              </p>
+              <div className="provider-container">
+                <Avatar src={news.source_favicon_url} alt="news" />
+                <Text className="provider-name">{news.source_url}</Text>
+                <Text>
+                  {moment(news.published_datetime_utc).startOf("ss").fromNow()}
+                </Text>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
     </Row>
-    )
-}
+  );
+};
 
-export default News
+export default News;
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -1,23 +1,25 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const cryptoNewsApiHeaders = { 
-    'X-BingApis-SDK': 'true',
-    'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com',
-    'X-RapidAPI-Key': '3a7379dab6msh2377a432c1c9545p1f7e1djsn509e0b534fbc'
-}
+const cryptoNewsApiHeaders = {
+  "X-RapidAPI-Key": "3a7379dab6msh2377a432c1c9545p1f7e1djsn509e0b534fbc",
+  "X-RapidAPI-Host": "real-time-news-data.p.rapidapi.com",
+};
 
-const baseUrl = 'https://bing-news-search1.p.rapidapi.com';
+const baseUrl = "https://real-time-news-data.p.rapidapi.com";
 
-const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders});
+const createRequest = (url) => ({ url, headers: cryptoNewsApiHeaders });
 
 export const cryptoNewsApi = createApi({
-    reducerPath: "cryptoNewsApi", 
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptoNews: builder.query({
-            query: ( {newsCategory, count} ) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
-        })
-    })
+  reducerPath: "cryptoNewsApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptoNews: builder.query({
+      query: ({ newsCategory, count }) =>
+        createRequest(
+          `/search?query=${newsCategory}&country=US&lang=en&count=${count}`
+        ),
+    }),
+  }),
 });
 
 export const { useGetCryptoNewsQuery } = cryptoNewsApi;
