41cd9a86d5615b5d6b9a5f43e174b65ff012b792
davidh1064 <huangdavid10@gmail.com>
2025-04-02 13:16:39 -0700

    added env.example file and update .gitignore; refactor cryptoApi to use env variables

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,25 +1,22 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const cryptoApiHeaders = {
-    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
-    'X-RapidAPI-Key': '3a7379dab6msh2377a432c1c9545p1f7e1djsn509e0b534fbc'
-}
+  "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+  "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+};
 
 const baseUrl = "https://coinranking1.p.rapidapi.com";
 
-const createRequest = (url) => ({ url, headers: cryptoApiHeaders});
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
-    reducerPath: "cryptoApi", 
-    baseQuery: fetchBaseQuery({ baseUrl }),
-    endpoints: (builder) => ({
-        getCryptos: builder.query({
-            query: (count) => createRequest(`/coins?limit=${count}`),
-        })
-    })
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: (count) => createRequest(`/coins?limit=${count}`),
+    }),
+  }),
 });
 
-export const {
-    useGetCryptosQuery,
-} = cryptoApi;
-
+export const { useGetCryptosQuery } = cryptoApi;
